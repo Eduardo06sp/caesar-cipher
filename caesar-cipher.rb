2fc0fe9e17c96a892beed0ad621b547a7da14474
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-01-06 13:40:00 -0800

    Store lowercase and uppercase alphabet letters in arrays

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -4,4 +4,8 @@ original_string = gets.chomp
 puts 'Please enter the encryption key (number 0-25)'
 encryption_key = gets.chomp
 
+lowercase_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+uppercase_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 puts result
